452017622fb40a784c5ef91eb53bf6f476b649c0
Patricio Lumbenlengo <patriciolumbee@gmail.com>
2026-02-23 13:36:15 +0100

    Implement full networking with public and private subnets

--- main.tf
+++ main.tf
@@ -8,3 +8,83 @@ resource "aws_vpc" "main" {
     Name = "${var.project_name}-vpc"
   }
 }
+
+# 1. Internet Gateway The "Front Door" to the Internet
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "lumbenlengo-igw"
+  }
+}
+
+# 2. Public Subnet 1 (Availability Zone A)
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "lumbenlengo-public-1"
+  }
+}
+
+# 3. Public Subnet 2 (Availability Zone B High Availability)
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "lumbenlengo-public-2"
+  }
+}
+
+# 4. Route Table  The "GPS" for network traffic
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "lumbenlengo-public-rt"
+  }
+}
+
+# 5. Route Table Association Connecting Subnets to the Route Table
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 6. Private Subnet 1 (Zona A)
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "lumbenlengo-private-1"
+  }
+}
+
+# 7. Private Subnet 2 (Zona B)
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "lumbenlengo-private-2"
+  }
+}
