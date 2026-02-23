72637e633ea6a50a965115729ff2e6e1024b4e2f
Patricio Lumbenlengo <patriciolumbee@gmail.com>
2026-02-23 14:16:08 +0100

    Deploy security group and ec2 instance

--- main.tf
+++ main.tf
@@ -88,3 +88,83 @@ resource "aws_subnet" "private_2" {
     Name = "lumbenlengo-private-2"
   }
 }
+
+
+
+# SECURITY GROUP CONFIGURATION
+
+
+# Resource: AWS Security Group
+# Purpose: Acts as a virtual firewall for the EC2 Instance
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-web-sg"
+  description = "Allow HTTP and SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  # Inbound Rule: HTTP
+  # Port: 80
+  # Purpose: Allows public web traffic to reach the web server
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound Rule: SSH
+  # Port: 22
+  # Purpose: Allows administrative access via Terminal
+  # Security Note: Using 0.0.0.0/0 is risky; in production, use your specific IP.
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound Rule: All Traffic
+  # Purpose: Allows the server to initiate connections to the internet
+  # Examples: Downloading OS updates, security patches, or connecting to S3.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 means ALL protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-web-sg"
+  }
+}
+
+
+# EC2 INSTANCE CONFIGURATION
+
+
+resource "aws_instance" "web_server" {
+  # Amazon Linux 2023 AMI (North Virginia)
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t2.micro"
+
+  # Networking settings
+  # Linking the instance to one of our public subnets
+  subnet_id = aws_subnet.public_1.id
+
+  # Attaching the Security Group we just planned
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  # Assigning a Public IP so we can access it
+  associate_public_ip_address = true
+
+  # Storage configuration (Best Practice: gp3)
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.project_name}-web-server"
+  }
+}
