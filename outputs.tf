c9efab21a187e69b431d448d8f505925dae4de06
Patricio Lumbenlengo <patriciolumbee@gmail.com>
2026-02-25 23:59:34 +0100

    add ec2 and subnet details to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,28 @@ output "vpc_cidr" {
   description = "CIDR block da VPC"
   value       = aws_vpc.main.cidr_block
 }
+
+# COMPUTE OUTPUTS
+
+output "ec2_public_ip" {
+  description = "The Public IP address of the web server"
+  value       = aws_instance.web_server.public_ip
+}
+
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web_server.id
+}
+
+
+# NETWORK OUTPUTS
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
