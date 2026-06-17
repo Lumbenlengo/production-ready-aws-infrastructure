edb77b58e95643046b850028d79b349c12977f58
Patricio Lumbenlengo <patriciolumbee@gmail.com>
2026-06-17 13:42:18 +0000

    fix: increase timeout for security hub and apply cleanup

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -98,6 +98,9 @@ resource "aws_securityhub_standards_subscription" "cis" {
 resource "aws_securityhub_standards_subscription" "aws_foundational" {
   standards_arn = "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0"
   depends_on    = [aws_securityhub_account.main]
+  timeouts {
+    create = "15m"
+  }
 }
 
 # IAM Access Analyzer 
